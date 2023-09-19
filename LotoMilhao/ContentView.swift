a85cc0ee8bb340d206cb213fa67770df96a48bfe
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2023-09-19 16:07:23 -0300

    Initial Commit

---
+++ LotoMilhao/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LotoMilhao
+//
+//  Created by Leandro Morais on 19/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LotoMilhao/LotoMilhaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotoMilhaoApp.swift
+//  LotoMilhao
+//
+//  Created by Leandro Morais on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LotoMilhaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
